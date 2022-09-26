4e486166a8efe89e567335c9a9c833b5f823d729
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-09-26 19:25:05 +0600

    Card Calculation added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+
+    // calculating totaol price , shipping,tax, grand total
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = parseInt((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
+
     return (
-        <div>
+        <div className='cart'>
             <h3>Order summary</h3>
             <p>Selected items: {cart.length}</p>
+            <p>Total Price: ${total} </p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal} </h5>
         </div>
     );
 };
