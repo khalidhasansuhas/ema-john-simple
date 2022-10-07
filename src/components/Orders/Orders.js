d421563201b705dd3ca50dac9d9168ab28f5a924
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-10-07 23:23:25 +0600

    implemented review item style

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const {products,initialCart} = useLoaderData(); //{products: products, initialCart : initialCart}
     const [cart, setCart] = useState(initialCart);
     return (
         <div className='shop-container'>
-            <div className='products-container'>
-
+            <div className='orders-container'>
+                {
+                    cart.map(product => <ReviewItem
+                    key={product.id}
+                    product={product}
+                    ></ReviewItem>)
+                }
             </div>
             <div className='cart-container'>
                 <Cart cart={cart}></Cart>
