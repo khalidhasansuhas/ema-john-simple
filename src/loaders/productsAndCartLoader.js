969547ee17c7a5a0ab44a0166ea5a9b9e7540188
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-10-07 23:01:28 +0600

    imported cart from loader and displayed shopping cart

--- src/loaders/productsAndCartLoader.js
+++ src/loaders/productsAndCartLoader.js
@@ -7,5 +7,14 @@ export const productsAndCartLoader = async ()=>{
 
     //get cart
     const savedCart = getStoredCart();
-    return products;
+    const initialCart =[]
+    for(const id in savedCart ){
+        const addedProduct = products.find(product =>product.id ===id);
+        if(addedProduct){
+            const quantity = savedCart[id];
+            addedProduct.quantity = quantity;
+            initialCart.push(addedProduct);
+        }
+    }
+    return {products: products, initialCart : initialCart};
 }
