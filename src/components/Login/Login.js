9259ac422c3147aabfb75bbaeb9ff33160c33035
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-10-21 10:54:18 +0600

    8 After Login Redirect Navigate to the right route

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/UserContext';
 import './Login.css';
 
@@ -7,6 +7,8 @@ const Login = () => {
 
     const { signIn } = useContext(AuthContext);
 const navigate = useNavigate();
+const location = useLocation();
+const from = location.state?.from?.pathname || '/'
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -19,7 +21,7 @@ const navigate = useNavigate();
                 const user = result.user;
                 console.log(user);
                 form.reset();
-                navigate('/');
+                navigate(from, {replace: true});
             })
             .catch(error => console.error(error))
     }
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -7,28 +7,33 @@ const auth = getAuth(app)
 
 const UserContext = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth , email, password)
     }
 
     const signIn = (email, password)=>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     const logOut =()=>{
+        setLoading(true);
         return signOut(auth);
     }
 
     useEffect(()=>{
        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
             console.log('current user inside state change ',currentUser);
-            setUser(currentUser)
+            setUser(currentUser);
+            setLoading(false);
         })
         return ()=> unsubscribe();
     },[])
 
-    const authInfo = {user , createUser, signIn, logOut}
+    const authInfo = {user ,loading, createUser, signIn, logOut}
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
